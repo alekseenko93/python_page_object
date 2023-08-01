8289d05d5e2d0cc33b63f008e866b915610530fa
alekseenko93 <irina_manova@mail.ru>
2023-08-01 16:39:45 +0500

    conftest changed to start maximized browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,12 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-"""данная функция-фикстура будет исполнятся только 1 раз за тестовую сессию"""
-
 
 @pytest.fixture(scope="session")
 def browser_init():
     service = Service(executable_path="../chromedriver.exe")
     options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
